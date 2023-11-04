c6fceb9cdf8f881cf90006039b11af928f46aa2e
Baba <dsmwaura@gmail.com>
2023-11-04 13:52:03 +0300

    Added NOK functionality

--- client_registry/api/hie_registry.py
+++ client_registry/api/hie_registry.py
@@ -4,13 +4,15 @@ import jwt
 
 
 @frappe.whitelist()
-def patient_lookup(payload, page_length=20):
+def client_lookup(payload, page_length=20):
 	result = []
 	if isinstance(payload, str):
 		payload =json.loads(payload)
 	or_filters = payload
 	filters_from_user =  list(dict.fromkeys(or_filters))
 	if(len(filters_from_user)<1): return dict(status="error",description="Search filters not provided")
+	if "id" in filters_from_user:
+		or_filters["name"] = payload.pop("id")
 	# return frappe.get_all("Client Registry", or_filters=or_filters, fields=["first_name","middle_name","last_name","full_name","gender","date_of_birth","identification_type","identification_number","is_alive","deceased_datetime","phone","email","country","county","sub_county","ward","village","related_to","related_to_full_name"])
 	records = frappe.get_all("Client Registry",filters=or_filters, page_length=page_length)
 	for record in records:
@@ -18,7 +20,7 @@ def patient_lookup(payload, page_length=20):
 		result.append(doc.to_fhir())
 	return result
 @frappe.whitelist()
-def create_patient(payload):
+def create_client(payload):
 	# return type(payload)
 	if isinstance(payload, str):
 		payload =json.loads(payload)
@@ -27,9 +29,10 @@ def create_patient(payload):
 	identifiers =  payload.pop("identifier")
 	payload["registry_system"] = identifiers.get("system") or ""
 	payload["record_id"] = identifiers.get("value") or ""
-	id_payload = dict(identity="{}:{}".format(payload.get("identification_type").lower(), payload.get("identification_number").lower()).replace(" ","_"))
-	encoded_jwt = jwt.encode(id_payload, "secret", algorithm="HS256")
-	payload["id_hash"] = encoded_jwt
+	if not payload.get("related_to"):
+		id_payload = dict(identity="{}:{}".format(payload.get("identification_type").lower(), payload.get("identification_number").lower()).replace(" ","_"))
+		encoded_jwt = jwt.encode(id_payload, "secret", algorithm="HS256")
+		payload["id_hash"] = encoded_jwt
 	doc = frappe.get_doc(payload).insert()
 	frappe.db.commit()
 	return doc.to_fhir()
@@ -39,11 +42,25 @@ def update_patient(payload):#TBD
 def validate_resource_type(resource):
 	if not resource.get("Patient"):
 		return dict(status="error",error_desc="Invalid resource")
-	# "dependants":[
-	#     "first_name":,
-	#     "middle_name",
-	#     "last_name",
-	#     "gender",
-	#     "relation",
-	#     "linked_record"
-	# ]  
+def update_dependants(doc, state):
+	if not doc.get("related_to"): return
+	is_dependant_of_doc = frappe.get_doc("Client Registry",doc.get("related_to")) 
+	client_dependants = is_dependant_of_doc.get("dependants")
+	records = [x.get("linked_record") for x in client_dependants]
+	if doc.get("name") in records: return
+	is_dependant_of_doc.append("dependants", {
+		"first_name": doc.get("first_name"),
+		"middle_name": doc.get("middle_name") or "",
+		"last_name": doc.get("last_name"),
+		"gender": doc.get("gender"),
+		"relationship": doc.get("relationship"),
+		"identification_type": doc.get("identification_type"),
+		"identification_number": doc.get("identification_number"),
+		"linked_record": doc.get("name")
+	})
+	is_dependant_of_doc.save()
+	
+	
+	
+
+	  
